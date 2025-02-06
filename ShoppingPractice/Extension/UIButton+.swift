065e92ed25abeb5287847ee2a29b9d74d3cb20f4
BAE <lexon1209@gmail.com>
2025-02-07 02:29:43 +0900

    feat: 버튼 스타일 extension

---
+++ ShoppingPractice/Extension/UIButton+.swift
@@ -0,0 +1,22 @@
+//
+//  UIButton+.swift
+//  ShoppingPractice
+//
+//  Created by BAE on 2/7/25.
+//
+
+import UIKit
+
+extension UIButton.Configuration {
+    static func sortButtonStyle(title: String) -> UIButton.Configuration {
+        var attribString = AttributedString(title)
+        attribString.font = .systemFont(ofSize: 16)
+        var config = UIButton.Configuration.plain()
+        config.attributedTitle = attribString
+        config.baseForegroundColor = .white
+        config.background.backgroundColor = .black
+        config.background.strokeWidth = 1
+        config.background.strokeColor = .white
+        return config
+    }
+}
