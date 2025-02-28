2ad9963030a60610b684e8648d7b8766b0603eb1
BAE <lexon1209@gmail.com>
2025-03-01 02:35:10 +0900

    chore: 주석 정리

--- ShoppingPractice/Presentation/ShoppingList/ShoppingListViewModel.swift
+++ ShoppingPractice/Presentation/ShoppingList/ShoppingListViewModel.swift
@@ -31,7 +31,6 @@ final class ShoppingListViewModel: ViewModel {
         let searchResult: Driver<[MerchandiseInfo]>
         let totalNumber: Driver<String?>
         let errorNoti: Driver<String>
-//        let vc: Driver<ShoppingListViewController>
     }
     
     func transform(input: Input) -> Output {
@@ -117,11 +116,6 @@ final class ShoppingListViewModel: ViewModel {
             }
             .disposed(by: disposeBag)
         
-//        input.itemSelect
-//            .bind { item in
-//                shoppingListVC.accept(.)
-//            }
-        
         return Output(navTitle: searchKeyword.asDriver(),
                       searchResult: result.asDriver(onErrorDriveWith: .empty()),
                       totalNumber: totalNumber.asDriver(onErrorDriveWith: .empty()),
@@ -129,50 +123,7 @@ final class ShoppingListViewModel: ViewModel {
         )
     }
     
-    // MARK: - given properties
-//    var lastSearched = ""
-//    var lastFilter: SortBy = .sim
-    
-    // MARK: - properties
     let spacing: CGFloat = 12
-//    var currentPage: Int {
-//        return outputShoppingList.value.count
-//    }
-//    
-//    // MARK: - observed properties
-//    let inputSearchText: CustomObservable<String?> = CustomObservable(nil)
-//    let inputShoppingList: CustomObservable<Merchandise?> = CustomObservable(nil)
-//    let inputPrefetching: CustomObservable<Void?> = CustomObservable(())
-//    let inputSortButtonTapped: CustomObservable<SortBy?> = CustomObservable(nil)
-//    
-//    let outputShoppingList: CustomObservable<[MerchandiseInfo]> = CustomObservable([])
-//    let outputTotalCount = CustomObservable("")
-//    var outputIsSorted = CustomObservable(false)
-    
-//    init() {
-//        inputShoppingList.bind { [weak self] response in
-//            if let response = response, let total = self?.configTotalCount(total: response.total) {
-//                self?.outputShoppingList.value = response.items
-//                self?.outputTotalCount.value = total
-//            }
-//        }
-//        inputPrefetching.lazyBind { [weak self] _ in
-//            self?.callSortedRequest()
-//        }
-//        inputSortButtonTapped.lazyBind { [weak self] sortby in
-//            self?.outputIsSorted.value.toggle()
-//            if self?.lastFilter == sortby {
-//                return
-//            } else {
-//                if let lastSearched = self?.lastSearched, let sortby = sortby {
-//                    ShoppingService.shared.callSearchReQuest(api: .sorted(keyword: lastSearched, sortby: sortby), type: Merchandise.self) { response in
-//                        self?.outputShoppingList.value = response.items
-//                        self?.lastFilter = sortby
-//                    }
-//                }
-//            }
-//        }
-//    }
     
     private func configTotalCount(total: Int) -> String? {
         let formatter = NumberFormatter()
@@ -180,11 +131,4 @@ final class ShoppingListViewModel: ViewModel {
         return formatter.string(for: total)
     }
     
-//    private func callSortedRequest() {
-////        print(#function, "프리패칭 리퀘스트 콜!!")
-//        ShoppingService.shared.callSearchReQuest(api: .sorted(keyword: lastSearched, sortby: lastFilter, startAt: currentPage), type: Merchandise.self) { [weak self] response in
-//            self?.outputShoppingList.value.append(contentsOf: response.items)
-//        }
-//        
-//    }
 }
